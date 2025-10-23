bc2c1c4eba3a33852428fe2061f0b7e6781096ba
Nic Ficca <nic@fossa.com>
2025-10-23 14:01:18 -0400

    Correct comment

--- locator/src/purl/sourceforge.rs
+++ locator/src/purl/sourceforge.rs
@@ -5,7 +5,7 @@ pub fn purl_to_locator(purl: Purl) -> Result<Locator, super::Error> {
     // pkg:sourceforge/pkgname@1.0
     // or
     // pkg:sourceforge/pkgname/subpkgname@1.0
-    // If there's a namespace, use it; otherwise use the name.
+    // if there is no subpkgname, then the namespace will be the name.
     let package_name = purl.namespace().unwrap_or(purl.name());
     let revision = purl.version().and_then(|v| Revision::parse(v).ok());
 
