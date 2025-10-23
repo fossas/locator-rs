0a3959a60a78571f04e023bae1be5b7cbbb2e2a6
Nic Ficca <nic@fossa.com>
2025-10-23 14:04:35 -0400

    Clippy

--- locator/src/purl/composer.rs
+++ locator/src/purl/composer.rs
@@ -1,9 +1,11 @@
 use crate::{Locator, Revision, ecosystems::Comp, purl::Purl};
 
 pub fn purl_to_locator(purl: Purl) -> Result<Locator, super::Error> {
-    let namespace = purl
-        .namespace()
-        .ok_or_else(|| super::Error::MissingNamespace("Composer PURL requires a namespace (vendor name)".to_string()))?;
+    let namespace = purl.namespace().ok_or_else(|| {
+        super::Error::MissingNamespace(
+            "Composer PURL requires a namespace (vendor name)".to_string(),
+        )
+    })?;
 
     let package_name = format!("{}/{}", namespace, purl.name());
     let revision = purl.version().and_then(|v| Revision::parse(v).ok());
--- locator/src/purl/swift.rs
+++ locator/src/purl/swift.rs
@@ -1,9 +1,9 @@
 use crate::{Locator, Revision, ecosystems::Swift, purl::Purl};
 
 pub fn purl_to_locator(purl: Purl) -> Result<Locator, super::Error> {
-    let namespace = purl
-        .namespace()
-        .ok_or_else(|| super::Error::MissingNamespace("Swift PURL requires a namespace".to_string()))?;
+    let namespace = purl.namespace().ok_or_else(|| {
+        super::Error::MissingNamespace("Swift PURL requires a namespace".to_string())
+    })?;
 
     let package_name = format!("{}/{}", namespace, purl.name());
     let revision = purl.version().and_then(|v| Revision::parse(v).ok());
